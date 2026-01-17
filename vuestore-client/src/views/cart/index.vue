d6653829fcab28dc066405b0de2d284900c393fc
Valentino banyu ampar paraya <144188174+nyukkiw@users.noreply.github.com>
2026-01-17 21:15:15 +0700

    reusable component and props

--- vuestore-client/src/views/cart/index.vue
+++ vuestore-client/src/views/cart/index.vue
@@ -4,19 +4,11 @@
             <h1>
                 Shopping cart
             </h1>
-            <div 
+            <ItemCart 
             v-for="item in cartItems"
             :key="item.id"
-            class="product-container"
-            
-            >
-                <img :src="item.imageUrl" :alt="item.name" class="product-image">
-                <div class="details-wrap">
-                    <h3>{{ item.name }}</h3>
-                    <p>Rp.{{ item.price }}</p>
-                </div>
-                <button class="remove-button">Remove</button>
-            </div>
+            :item="item"
+            />
             <h3 id="total-price">Total:Rp{{ totalPrice }}</h3>
             <button id="checkout-button">Checkout Button</button>
         </div>
@@ -25,9 +17,13 @@
 
 <script>
 import {cartItems} from '../../data-seed'
+import itemCart from '../../components/itemCart.vue'
 
 export default {
  name: 'CartIndex',
+  components:{
+      itemCart
+  },
  data(){
     return {
         cartItems
@@ -58,24 +54,5 @@ export default {
   #checkout-button {
     width: 100%;
   }
-  .product-container {
-    align-content: 'center';
-    border-bottom: 1px solid #ddd;
-    display: flex;
-    padding: 16px;
-    width: 100%;
-  }
-  .product-image {
-    flex: 1;
-    height: 100px;
-    max-width: 100px;
-  }
-  .details-wrap {
-    padding: 0 16px;
-    flex: 3;
-  }
-  .remove-button {
-    flex: 1;
-    margin: auto;
-  }
+  
 </style>
--- vuestore-client/src/views/product/index.vue
+++ vuestore-client/src/views/product/index.vue
@@ -2,19 +2,11 @@
     <div>
         <div id="page-wrap">
             <div class="grid-wrap">
-                <div
+                <productItem
                     v-for="product in products"
                     :key="product.id"
-                    class="product-item"
-                >
-                    <img :src="product.imageUrl" alt="">
-                    <h3 class="product-name">{{ product.name }}</h3>
-                    <p class="product-price">Rp{{ product.price }}</p>
-                    <router-link  :to="{ name: 'product-Detail', params:{id: product.id} }">
-                        <button>Detail</button>
-                    </router-link>
-
-                </div>
+                    :product="product"
+                />
             </div>
         </div>
     </div>
@@ -22,14 +14,18 @@
 
 <script>
 import {products} from '../../data-seed'
-
+import productItem from '../../components/productItem.vue'
 export default {
     name: 'ProductIndex',
+    components:{
+        productItem
+    },
     data(){
         return {
             products
         }
-    }
+    },
+
 }
 </script>
 
@@ -41,33 +37,4 @@ export default {
     margin-top: 16px;
 }
 
-.product-item {
-    align-items: center;
-    border-radius: 8px;
-    box-shadow: 0px 2px 5px #888;
-    display: flex;
-    flex-direction: column;
-    margin-bottom: 2%;
-    padding: 20px;
-    position: relative;
-    width: 32%;
-}
-
-.product-name {
-    margin-bottom: 0;
-}
-
-img {
-    height: 130px;
-    width: 130px;
-    border-radius: 5%;
-}
-
-a {
-    width: 100%;
-}
-
-button {
-    width: 100%;
-}
 </style>
