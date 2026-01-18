298c761140d2f980c865686f02e5b4e6c18ba73e
Valentino banyu ampar paraya <144188174+nyukkiw@users.noreply.github.com>
2026-01-18 10:52:16 +0700

    Setup backend server and root gitignore

---
+++ vuestore-server/app.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 8000;
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+const db = require('./app/models');
+db.mongoose.connect(db.url)
+.then((result)=> {
+    console.log("database connected");
+}).catch((err) => {
+    console.log("cannot connect to the database!", err);
+    process.exit();
+});
+
+
+app.get('/',(req, res) => {
+    res.json({
+        message:'welcome to vuestore server'
+    });
+});
+
+app.listen(PORT, () => {
+    console.log(`server is running on port ${PORT}`)
+})
+
