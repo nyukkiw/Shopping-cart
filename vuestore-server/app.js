39b397ac44d9d64cdd4dcdb9dc9a3d6f1a737e63
Valentino banyu ampar paraya <144188174+nyukkiw@users.noreply.github.com>
2026-01-18 21:01:28 +0700

    made a route and insert mongo data

--- vuestore-server/app.js
+++ vuestore-server/app.js
@@ -21,6 +21,8 @@ app.get('/',(req, res) => {
     });
 });
 
+require('./app/routes/product.route')(app);
+
 app.listen(PORT, () => {
     console.log(`server is running on port ${PORT}`)
 })
---
+++ vuestore-server/app/routes/product.route.js
@@ -0,0 +1,7 @@
+module.exports = (app) => {
+    const products = require('../controllers/product.controller');
+    const router =  require('express').Router();
+
+    router.get('/', products.findAll);
+    app.use('/api/products', router);
+}
